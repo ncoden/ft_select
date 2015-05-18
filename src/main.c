8f0bc1a068b535a3e8960bd88e2007d21aeb41e9
Nicolas Coden <nicos.coden@neuf.fr>
2015-05-18 02:03:57 +0200

    Test recursive term loop

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/03 13:39:59 by ncoden            #+#    #+#             */
-/*   Updated: 2015/05/17 22:09:17 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/05/18 01:35:52 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,29 @@ int				main(int argc, char **argv)
 	(void)argc;
 	(void)argv;
 	t_trm		*trm;
+	t_trm		*trm2;
 
 	trm = ft_trmnew();
 	trm->opts.c_lflag &= ~(ICANON);
 	trm->opts.c_lflag &= ~(ECHO);
 	trm->opts.c_cc[VMIN] = 1;
 	trm->opts.c_cc[VTIME] = 0;
-	ft_trmkeyhook(trm, "c", (void (*)(void *))&ft_putstr, "Hello");
+	trm2 = ft_trmnew();
+	trm2->opts.c_lflag &= ~(ICANON);
+	trm2->opts.c_lflag &= ~(ECHO);
+	trm2->opts.c_cc[VMIN] = 1;
+	trm2->opts.c_cc[VTIME] = 0;
+	ft_trmkeyhook(trm, "c", (void (*)(void *))&ft_putstr, "PRESS1");
 	ft_trmkeycmd(trm, "e", "cl");
+	ft_trmkeyhook(trm, "n", (void (*)(void *))&ft_trmloop, trm2);
+	ft_trmsignalhook(trm, SIGWINCH, (void (*)(void *))&ft_putstr, "RESIZE1");
+	ft_trmkeyhook(trm, "s", (void (*)(void *))&ft_trmstop, trm);
+
+	ft_trmkeyhook(trm2, "c", (void (*)(void *))&ft_putstr, "PRESS2");
+	ft_trmkeycmd(trm2, "e", "cl");
+	ft_trmkeyhook(trm2, "n", (void (*)(void *))&ft_trmloop, trm);
+	ft_trmsignalhook(trm2, SIGWINCH, (void (*)(void *))&ft_putstr, "RESIZE2");
+	ft_trmkeyhook(trm2, "s", (void (*)(void *))&ft_trmstop, trm2);
 	ft_trmloop(trm);
 	return(0);
 }
