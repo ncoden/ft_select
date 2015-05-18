9322e6218d16a98c8750c771e1c88850a89eeba9
Nicolas CODEN <nicos.coden@neuf.fr>
2015-05-18 19:34:54 +0200

    Print to ft_trmgetout() instead of 0

--- src/main.c
+++ src/main.c
@@ -6,12 +6,13 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/03 13:39:59 by ncoden            #+#    #+#             */
-/*   Updated: 2015/05/18 01:35:52 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/05/18 19:34:20 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_select.h"
 	#include <stdio.h>
+
 int				main(int argc, char **argv)
 {
 	(void)argc;
@@ -29,17 +30,20 @@ int				main(int argc, char **argv)
 	trm2->opts.c_lflag &= ~(ECHO);
 	trm2->opts.c_cc[VMIN] = 1;
 	trm2->opts.c_cc[VTIME] = 0;
-	ft_trmkeyhook(trm, "c", (void (*)(void *))&ft_putstr, "PRESS1");
+	ft_trmkeyhook(trm, "c", (void (*)(void *))&ft_putstr_trm, "PRESS1");
 	ft_trmkeycmd(trm, "e", "cl");
 	ft_trmkeyhook(trm, "n", (void (*)(void *))&ft_trmloop, trm2);
-	ft_trmsignalhook(trm, SIGWINCH, (void (*)(void *))&ft_putstr, "RESIZE1");
+	ft_trmsignalhook(trm, SIGWINCH, (void (*)(void *))&ft_putstr_trm, "RESIZE1");
 	ft_trmkeyhook(trm, "s", (void (*)(void *))&ft_trmstop, trm);
 
-	ft_trmkeyhook(trm2, "c", (void (*)(void *))&ft_putstr, "PRESS2");
+	ft_trmkeyhook(trm2, "c", (void (*)(void *))&ft_putstr_trm, "PRESS2");
 	ft_trmkeycmd(trm2, "e", "cl");
 	ft_trmkeyhook(trm2, "n", (void (*)(void *))&ft_trmloop, trm);
-	ft_trmsignalhook(trm2, SIGWINCH, (void (*)(void *))&ft_putstr, "RESIZE2");
+	ft_trmsignalhook(trm2, SIGWINCH, (void (*)(void *))&ft_putstr_trm, "RESIZE2");
 	ft_trmkeyhook(trm2, "s", (void (*)(void *))&ft_trmstop, trm2);
+
+	ft_putstr("lanched > ");
 	ft_trmloop(trm);
+	ft_trmcloseout();
 	return(0);
 }
