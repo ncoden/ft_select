c4a8325d21a6fe7ee651fbee8e734291fa845294
Nicolas CODEN <nicos.coden@neuf.fr>
2015-05-30 00:43:13 +0200

    Update main for new features

--- src/main.c
+++ src/main.c
@@ -6,12 +6,19 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/03 13:39:59 by ncoden            #+#    #+#             */
-/*   Updated: 2015/05/24 20:46:57 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/05/30 00:12:29 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_select.h"
 	#include <stdio.h>
+
+static void		trm_start(t_mt_tps *tps)
+{
+	ft_putstr("\nNew terminal (");
+	ft_putullonghex((unsigned long long)tps);
+	ft_putstr(") > ");
+}
 
 int				main(int argc, char **argv)
 {
@@ -30,20 +37,20 @@ int				main(int argc, char **argv)
 	trm2->opts.c_lflag &= ~(ECHO);
 	trm2->opts.c_cc[VMIN] = 1;
 	trm2->opts.c_cc[VTIME] = 0;
-	ft_trmkeyhook(trm, "c", (void (*)(void *))&ft_putstr_trm, "PRESS1");
-	ft_trmkeycmd(trm, "e", "cl");
-	ft_trmkeyhook(trm, "n", (void (*)(void *))&ft_trmstart, trm2);
-	ft_trmsignalhook(trm, SIGWINCH, (void (*)(void *))&ft_putstr_trm, "RESIZE1");
-	ft_trmkeyhook(trm, "s", (void (*)(void *))&ft_trmstop, trm);
-	ft_trmkeyhook(trm, "t", (void (*)(void *))&ft_readstop, NULL);
 
+	trm->on_start = &trm_start;
+	ft_trmkeyhook(trm, "c", (void (*)(void *))&ft_putstr_trm, "PRESS1");
+	ft_trmkeyhook(trm, "n", (void (*)(void *))&ft_trmstart, trm2);
+	ft_trmsignalhook(trm, SIGWINCH, (void (*)(void *))&ft_trmstop, NULL);
+	ft_trmkeyhook(trm, "s", (void (*)(void *))&ft_trmstop, NULL);
+
+	trm2->on_start = &trm_start;
 	ft_trmkeyhook(trm2, "c", (void (*)(void *))&ft_putstr_trm, "PRESS2");
-	ft_trmkeycmd(trm2, "e", "cl");
 	ft_trmkeyhook(trm2, "n", (void (*)(void *))&ft_trmstart, trm);
 	ft_trmsignalhook(trm2, SIGWINCH, (void (*)(void *))&ft_putstr_trm, "RESIZE2");
-	ft_trmkeyhook(trm2, "s", (void (*)(void *))&ft_trmstop, trm2);
+	ft_trmkeyhook(trm2, "s", (void (*)(void *))&ft_trmstop, NULL);
 
-	ft_putstr("lanched > ");
+	ft_putstr("[ lanched ]");
 	ft_trmstart(trm);
 	ft_trmcloseout();
 	return(0);
