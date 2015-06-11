4678779b8ce15bb3ff8e99eecc83938a620ec882
Nicolas Coden <ncoden@student.42.fr>
2015-06-11 16:17:26 +0200

    Fix cursor move functions
    (finish the work, see previous commit)

--- src/cursor.c
+++ src/cursor.c
@@ -6,21 +6,21 @@
 /*   By: ncoden <ncoden@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/06/10 15:49:30 by ncoden            #+#    #+#             */
-/*   Updated: 2015/06/10 17:40:50 by ncoden           ###   ########.fr       */
+/*   Updated: 2015/06/11 14:13:33 by ncoden           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_select.h"
 
-static int		list_last_col(t_lst_col *cols, int y)
+static int		distance_to_last_column(t_lst_col *col, int index)
 {
 	int			i;
 
-	i = y;
-	while (cols->next && cols->next->height > y)
+	i = 0;
+	while (col->next && col->next->height > index)
 	{
-		i += cols->height;
-		cols = cols->next;
+		i += col->height;
+		col = col->next;
 	}
 	return (i);
 }
@@ -48,15 +48,14 @@ void			cursor_go_left(t_select_list *list)
 	if (list->cursor > list->cols->height - 1)
 		list->cursor -= list->cols->height;
 	else
-		list->cursor = list_last_col(list->cols, list->cursor.y);
+		list->cursor += distance_to_last_column(list->cols, list->cursor);
 	args_print(list);
 }
 
 void			cursor_go_right(t_select_list *list)
 {
-	if (list->cursor < list_last_col(list->cols, list->cursor.y))
-		list->cursor += list->cols->height;
-	else
-		list->cursor = list->cursor % list->cols->height;
+	list->cursor += list->cols->height;
+	if (list->cursor >= ft_lstlen((t_lst *)list->cols->items))
+		list->cursor %= list->cols->height;
 	args_print(list);
 }
